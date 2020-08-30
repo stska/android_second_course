3bd94f0c827982b74a6923a310d1a9c4c643d47e
stska <33188480+stska@users.noreply.github.com>
2020-08-31 01:08:17 +0300

    lesson_two
    homework

--- app/src/main/java/com/example/weather_app_drawer_second_java/MainActivity.java
+++ app/src/main/java/com/example/weather_app_drawer_second_java/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.weather_app_drawer_second_java;
 
-import android.graphics.Color;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
-
-import com.example.weather_app_drawer_second_java.weatherApp.App;
-import com.example.weather_app_drawer_second_java.weatherApp.SharedPreferencesClass;
+import android.widget.CursorAdapter;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import com.example.weather_app_drawer_second_java.weatherApp.CityWeatherDescription;
+import com.example.weather_app_drawer_second_java.weatherApp.JsonCurrentClass.Example2;
+import com.example.weather_app_drawer_second_java.weatherApp.SingltoneListOfCities;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,21 +31,26 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-// TODO ps. just reminding (1°C × 9/5) + 32 = 33.8°F
+import java.io.IOException;
 
-/*
-TODO:  1)сделать и русский язык тоже 2)сделать, чтобы клавиатура не загораживала ввод 3) сделать, чтобы любимые города, при удалении сразу убирались из списка
-TODO:  4)Сделать чтение json файла городов при старте в другом потоке, чтобы не было белого экрана вначале или же чтение перенести в другое место и выводить загрузку, чтобы пользователь знад, что что-то происходит
-TODO 5)Сделать поиск с подсказкой
- */
-
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    SingltoneListOfCities singltoneListOfCities;
+    private FragmentActivity myContext;
+
+
+    private SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            singltoneListOfCities = SingltoneListOfCities.getInstance(getResources());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -48,8 +65,7 @@ public class MainActivity extends AppCompatActivity  {
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
@@ -57,21 +73,90 @@ public class MainActivity extends AppCompatActivity  {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            assert query != null;
+            Toast.makeText(MainActivity.this, query, Toast.LENGTH_LONG).show();
+        }
+
+        final String[] from = new String[]{"cityName"};
+        final int[] to = new int[]{android.R.id.text1};
+        mAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
+                null,
+                from,
+                to,
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.manu, menu);
 
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setSuggestionsAdapter(mAdapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                if (compareQueryToList(s, singltoneListOfCities.example2)) {
+                    goTo(s);
+                    clearTab(searchView);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                populateAdapter(s);
+                return false;
+            }
+
+        });
+        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+            @Override
+            public boolean onSuggestionSelect(int i) {
+                return true;
+            }
+
+            @Override
+            public boolean onSuggestionClick(int i) {
+                Cursor cursor = (Cursor) mAdapter.getItem(i);
+
+                if(cursor != null && cursor.moveToFirst()){
+                    goTo(cursor.getString(i));
+                }
+                clearTab(searchView);
+                return true;
+            }
+        });
         return true;
+    }
+
+    private void populateAdapter(String query) {
+        final MatrixCursor c = new MatrixCursor(new String[]{BaseColumns._ID, "cityName"});
+
+        for (int i = 0; i < singltoneListOfCities.getListOfCities().size(); i++) {
+            if (singltoneListOfCities.getListOfCities().get(i).toLowerCase().startsWith(query.toLowerCase()))
+                c.addRow(new Object[]{i, singltoneListOfCities.getListOfCities().get(i)});
+        }
+        mAdapter.changeCursor(c);
+
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        
+
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
@@ -79,8 +164,42 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    private boolean compareQueryToList(String query, Example2[] example2) {
+
+        for (int i = 0; i < example2.length; i++) {
+            if (example2[i].getName().equals(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void goTo(String city) {
+        CityWeatherDescription cwd = CityWeatherDescription.newInstance(city);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.initialFramLayoutId, cwd)
+                .addToBackStack(cwd.getClass().getName())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
 
     }
 
+    @Override
+    public void onBackPressed() {
+        int count = getFragmentManager().getBackStackEntryCount();
 
+        if (count == 0) {
+            super.onBackPressed();
+            recreate();     //если вы это видите, то подскажите насколько опасно так делать? незнал, как еще можно перерисовать активити не делая лисенеров и.т.д, так как уже время поздно.
+        } else {
+            getFragmentManager().popBackStack();
+        }
+    }
+    public void clearTab(SearchView searchView){
+        searchView.clearFocus();
+        searchView.setQuery("", false);
+        searchView.setIconified(true);
+    }
 }
